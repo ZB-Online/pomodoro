8ff94bda4876623f1cb6213ed4b0f1c139cd5c1d
zeromountain <sonyeongsan@gmail.com>
2021-12-09 16:19:59 +0900

    feat: update helper func

--- public/js/todos/reducer.js
+++ public/js/todos/reducer.js
@@ -12,34 +12,40 @@ const todos = [
 
 const WEEKS = ['sun', 'mon', 'tue', 'wed', 'thur', 'fri', 'sat'];
 
+const getDay = day => WEEKS.indexOf(day); // string 'mon' → number 1
+const getNum = day => WEEKS[day];
+
+// 새로운 todo 추가하면 기존 마지막 todo.id에 + 1
+// const generateId = () => Math.max(...todos.map(todo => todo.id), 0) + 1;
+
 // Date 객체 → 오늘 요일을 알아내는 함수
 const getDate = () => new Date().getDay();
 
 // filter by day of the week
-const filterByDay = (todos, day) => todos.filter(todo => todo.day === day);
+const filterByDay = (todos, day) => todos.filter(todo => todo.day === WEEKS[day]);
 
-// filter by uncompleted
-// 오늘 전날까지로 수정해야 함
-const filterByUncompleted = todos => todos.filter(todo => !todo.completed);
+// filter by uncompleted: 오늘 전날까지 todo 요소 중에서 completed가 false인 요소 필터
+const filterByUncompleted = todos => {
+  return todos.filter(todo => getDay(todo.day) < getDate()).filter(todo => !todo.completed);
+};
 
 // filter by completed
 const filterByCompleted = todos => todos.filter(todo => todo.completed);
 
-const getDay = day => WEEKS.indexOf(day);
-
 // filter id by uncompleted before day
 const getUncompletedId = todos => {
-  // day가 uncompleted인 경우 분리
-  const before = todos.filter(todo => getDay(todo.day) < getDate());
-  return before.filter(el => !el.completed);
+  // 1. 지난날짜만 뽑기
+  // 2. completed값이 false 인것만 뽑기
+  // 3. day가 uncompleted 아닌것만 뽑기
+  return todos
+    .filter(todo => getDay(todo.day) < getDate())
+    .filter(todo => !todo.completed)
+    .filter(todo => todo.day !== 'uncompleted');
 };
 
-// 새로운 todo 추가하면 기존 마지막 todo.id에 + 1
-const generateId = () => Math.max(...todos.map(todo => todo.id), 0) + 1;
-
-console.log(filterByDay(todos, 'uncompleted'));
+// console.log(filterByDay(todos, 'mon'));
 console.log(filterByUncompleted(todos));
-console.log(filterByCompleted(todos));
-console.log(generateId());
-getUncompletedId(todos);
-console.log(getUncompletedId(todos));
+// console.log(filterByCompleted(todos));
+// console.log(generateId());
+// getUncompletedId(todos);
+// console.log(getUncompletedId(todos));
